da20a5fe0f90032766548574483db84c8489bb5f
Trent Mentink <trentmentink@gmail.com>
2016-10-27 15:57:00 -0700

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+  // Configure task(s)
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      js: {
+        files: {
+          'src/js/application.js' : [ 'src/js/vendor/**/*.js', 
+                                      'src/js/base/*.js',
+                                      'src/js/component/*.js',
+                                      'src/js/section/*.js', 
+                                    ]
+        }
+      }
+    },
+    uglify: {
+      dev: {
+        options: {
+          beautify: {
+            beautify: true,
+            indent_level: 2,
+          },
+          mangle: false,
+          compress: false,
+          preserveComments: 'all'
+        },
+        src: 'src/js/application.js',
+        dest: 'dist/js/main.min.js'
+      },
+      dist: {
+        src: 'src/js/application.js',
+        dest: 'dist/js/main.min.js'
+      }
+    },
+    watch: {
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['concat','uglify:dev']
+      },
+      css: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['sass:dev']
+      }
+    } 
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register the task(s)
+  grunt.registerTask('default', ['concat','uglify:dev']);
+  grunt.registerTask('dist', ['concat','uglify:dist']);
+};
+
+
+
+
+
