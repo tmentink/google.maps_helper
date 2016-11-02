73aef1d596837183cb65340632792bc3afedf508
Trent Mentink <trentmentink@gmail.com>
2016-11-02 15:56:38 -0700

    created map module with an init method.

---
+++ src/js/component/map.js
@@ -0,0 +1,53 @@
+
+// ===========================================
+// Map
+// ===========================================
+  
+  var GMapsHelper = (function(GMH) {
+    "use strict";
+  
+    // Default Options
+    // =======================================
+    var _DEFAULTS = {
+      map: {
+        zoom: 6,
+        center: { lat: 37.5, lng: -120 },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+    }
+
+
+    // Google Maps Helper Object
+    // =======================================
+    GMH.Map = {};
+
+
+    // Init Map
+    // =======================================
+    var initMap = function(container, userOptions) {
+      try {
+        // get default options
+        var defaults = _DEFAULTS.map;
+
+        // combine user and default options
+        var options = $.extend({}, defaults, userOptions)
+       
+        // create new map and save reference
+        GMH.Data.Map = new google.maps.Map(document.getElementById(container), options);
+
+        return true;
+      }
+      catch (ex) {
+        console.log(ex);
+        return false;
+      }
+    }
+
+
+    // Public Methods
+    // =======================================
+    GMH.Map.init = initMap;
+
+
+    return GMH;
+  })(GMapsHelper || {});
