038f55d6cd81aa086177c239e2d6a4bea80fae44
Trent Mentink <trentmentink@gmail.com>
2017-03-18 20:51:08 -0700

    shortened object namespace and changed comment formatting to avoid conflicts. added ObjectArray base class

--- src/ts/data.ts
+++ src/ts/data.ts
@@ -1,18 +1,16 @@
-/// <reference path="object/labelArray.ts" />
-/// <reference path="object/markerArray.ts" />
-/// <reference path="object/polygonArray.ts" />
+/// <reference path="obj/labelArray.ts" />
+/// <reference path="obj/markerArray.ts" />
+/// <reference path="obj/polygonArray.ts" />
 
-/**
- * ------------------------------------------------------------------------
- * Google Maps Helper v1.0.0: data.ts 
- * ------------------------------------------------------------------------
- */
-
+// ------------------------------------------------------------------------
+// Google Maps Helper v1.0.0: data.ts 
+// ------------------------------------------------------------------------
+ 
 namespace GMH.Data {
 
   export const Map      = {}
-  export const Labels   = new GMH.Object.LabelArray()
-  export const Markers  = new GMH.Object.MarkerArray()
-  export const Polygons = new GMH.Object.PolygonArray()
+  export const Labels   = new GMH.Obj.LabelArray()
+  export const Markers  = new GMH.Obj.MarkerArray()
+  export const Polygons = new GMH.Obj.PolygonArray()
 
 }
