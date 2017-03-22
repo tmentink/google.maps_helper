331c901746e7091187aaed82cdc1952f3ea8ad30
Trent Mentink <trentmentink@gmail.com>
2017-03-21 20:28:11 -0700

    fixed package.json name. added clean, load-grunt-tasks and time-grunt. updated gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,31 @@
+
 module.exports = function(grunt) {
   var banner =  '/*!\n' +
-                ' * <%= pkg.name %> - v<%= pkg.version %> | <%= pkg.license %> License\n' +
+                ' * Google Maps Helper v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
                 ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-                ' * <%= pkg.homepage %>\n' +
+                ' * Licensed under <%= pkg.license %>\n' +
                 ' */\n'
 
-  // Configure task(s)
+  // Configure tasks
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      js: ['src/js/main.js', 'src/js/ts-compile.js', 'src/js/ts-compile.js.map']
+    },
     concat: {
       js: {
         files: {
           'src/js/main.js' : ['src/js/vendor/jquery-shim.js', 'src/js/ts-compile.js']
         }
+      }
+    },
+    ts: {
+      options: {
+        fast: 'never',
+        comments: true
+      },
+      dev: {
+        files: {'src/js/ts-compile.js': ['src/ts/**/*.ts']}
       }
     },
     uglify: {
@@ -28,42 +41,36 @@ module.exports = function(grunt) {
           banner: banner
         },
         src: 'src/js/main.js',
-        dest: 'dist/google.maps-helper.js',
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js',
       },
       dist: {
         options: {
           banner: banner
         },
         src: 'src/js/main.js',
-        dest: 'dist/google.maps-helper.min.js',
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.min.js',
       }
     },
     watch: {
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['concat', 'uglify:dev']
+      },
       ts: {
         files: ['src/ts/**/*.ts'],
         tasks: ['ts:dev','concat','uglify:dev']
       }
-    },
-    ts: {
-      options: {
-        fast: 'never',
-        comments: true
-      },
-      dev: {
-        files: {'src/js/ts-compile.js': ['src/ts/**/*.ts']}
-      }
     }
-  });
+  })
 
-  // Load the plugins
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-ts');
 
-  // Register the task(s)
-  grunt.registerTask('default', ['ts:dev','concat','uglify:dev']);
-  grunt.registerTask('dist', ['ts:dev','concat','uglify']);
-  grunt.registerTask('ts-compile', ['ts:dev']);
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt)
+  require('time-grunt')(grunt)
+
+
+  // Register tasks
+  grunt.registerTask('default', ['ts:dev', 'concat','uglify:dev', 'clean'])
+  grunt.registerTask('dist', ['ts:dev', 'concat','uglify', 'clean'])
+  grunt.registerTask('ts-compile', ['ts:dev'])
 };
-
