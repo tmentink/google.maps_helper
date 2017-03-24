fbb02c42f38bfdb8d29f13029037a44c1e55f7fe
Trent Mentink <trentmentink@gmail.com>
2017-03-24 09:28:04 -0700

    Updated util to read from constants namespace. Updated jQuery functions to avoid conflict.

--- src/ts/util.ts
+++ src/ts/util.ts
@@ -7,99 +7,6 @@
 namespace GMH.Util {
 
 
-  // ----------------------------------------------------------------------
-  // Constants 
-  // ----------------------------------------------------------------------
-
-  const EventType = {
-    ANIMATION_CHANGED   : "animation_changed",
-    BOUNDS_CHANGED      : "bounds_changed",
-    CENTER_CHANGED      : "center_changed",
-    CLICK               : "click",
-    CLICKABLE_CHANGED   : "clickable_changed",
-    CURSOR_CHANGED      : "cursor_changed",
-    DOUBLE_CLICK        : "dblclick",
-    DRAG                : "drag",
-    DRAG_END            : "dragend",
-    DRAG_START          : "dragstart",
-    DRAGGABLE_CHANGED   : "draggable_changed",
-    FLAT_CHANGED        : "flat_changed",
-    HEADING_CHANGED     : "heading_changed",
-    ICON_CHANGED        : "icon_changed",
-    IDLE                : "idle",
-    MAP_TYPE_ID_CHANGED : "maptypeid_changed",
-    MOUSE_DOWN          : "mousedown",
-    MOUSE_MOVE          : "mousemove",
-    MOUSE_OUT           : "mouseout",
-    MOUSE_OVER          : "mouseover",
-    MOUSE_UP            : "mouseup",
-    POSITION_CHANGED    : "position_changed",
-    PROJECTION_CHANGED  : "projection_changed",
-    RESIZE              : "resize",
-    RIGHT_CLICK         : "rightclick",
-    SHAPE_CHANGED       : "shape_changed",
-    TILES_LOADED        : "tilesloaded",
-    TILT_CHANGED        : "tilt_changed",
-    TITLE_CHANGED       : "title_changed",
-    VISIBLE_CHANGED     : "visible_changed",
-    ZINDEX_CHANGED      : "zindex_changed",
-    ZOOM_CHANGED        : "zoom_changed"
-  }
-
-  const EventTypeAlias = {
-    animationchanged  : EventType.ANIMATION_CHANGED,
-    boundschanged     : EventType.BOUNDS_CHANGED,
-    centerchanged     : EventType.CENTER_CHANGED,
-    click             : EventType.CLICK,
-    clickablechanged  : EventType.CLICKABLE_CHANGED,
-    cursorchanged     : EventType.CURSOR_CHANGED,
-    doubleclick       : EventType.DOUBLE_CLICK,
-    drag              : EventType.DRAG,
-    dragend           : EventType.DRAG_END,
-    dragstart         : EventType.DRAG_START,
-    draggablechanged  : EventType.DRAGGABLE_CHANGED,
-    flatchanged       : EventType.FLAT_CHANGED,
-    headingchanged    : EventType.HEADING_CHANGED,
-    iconchanged       : EventType.ICON_CHANGED,
-    idle              : EventType.IDLE,
-    maptypeidchanged  : EventType.MAP_TYPE_ID_CHANGED,
-    mousedown         : EventType.MOUSE_DOWN,
-    mousemove         : EventType.MOUSE_MOVE,
-    mouseout          : EventType.MOUSE_OUT,
-    mouseover         : EventType.MOUSE_OVER,
-    mouseup           : EventType.MOUSE_UP,
-    positionchanged   : EventType.POSITION_CHANGED,
-    projectionchanged : EventType.PROJECTION_CHANGED,
-    resize            : EventType.RESIZE,
-    rightclick        : EventType.RIGHT_CLICK,
-    shapechanged      : EventType.SHAPE_CHANGED,
-    tilesloaded       : EventType.TILES_LOADED,
-    tiltchanged       : EventType.TILT_CHANGED,
-    titlechanged      : EventType.TITLE_CHANGED,
-    visiblechanged    : EventType.VISIBLE_CHANGED,
-    zindexchanged     : EventType.ZINDEX_CHANGED,
-    zoomchanged       : EventType.ZOOM_CHANGED
-  }
-
-  const ObjectType = {
-    LABEL   : "Label",
-    MAP     : "Map",
-    MARKER  : "Marker",
-    POLYGON : "Polygon"
-  }
-
-  const ObjectTypeAlias = {
-    label    : ObjectType.LABEL,
-    labels   : ObjectType.LABEL,
-    map      : ObjectType.MAP,
-    maps     : ObjectType.MAP,
-    marker   : ObjectType.MARKER,
-    markers  : ObjectType.MARKER,
-    polygon  : ObjectType.POLYGON,
-    polygons : ObjectType.POLYGON
-  }
-
-
   // ------------------------------------------------------------------------
   // Functions 
   // ------------------------------------------------------------------------
@@ -111,13 +18,13 @@ namespace GMH.Util {
    */
   export function copy(source: GMH.Obj.ObjectArray, exclude?: any) {
     // make a deep copy of the source
-    const src_copy = $.extend(true, {}, source)
+    const src_copy = jQuery.extend(true, {}, source)
  
     // convert exclude into an array
-    if ($.type(exclude) == "object") {
+    if (jQuery.type(exclude) == "object") {
       exclude = Object.keys(exclude)
     }
-    else if ($.type(exclude) == "string") {
+    else if (jQuery.type(exclude) == "string") {
       exclude = exclude.split(",")
     }
 
@@ -136,14 +43,14 @@ namespace GMH.Util {
     const GMH_Obj = source.Type ? new GMH.Obj[source.Type] : {}
 
     // copy into new object 
-    return $.extend(GMH_Obj, src_copy)
+    return jQuery.extend(GMH_Obj, src_copy)
   }
 
   /**
    * Returns an array of the object's ids 
    * @param obj A GMH ObjectArray 
    */
-  export function getIDs(obj: GMH.Obj.ObjectArray) {
+  export function getIDs(obj: GMH.Obj.ObjectArray): string[] {
     let ids = Object.keys(obj)
 
     let _i = ids.indexOf("_i")
@@ -158,7 +65,7 @@ namespace GMH.Util {
    * Converts a comma separated string into a LatLng object 
    * @param str A comma separated string of x and y coordinates 
    */
-  export function toLatLng(str: string) {  
+  export function toLatLng(str: string): google.maps.LatLng {  
     const Delimeter = {
       LatLng: GMH.Config.Delimeter.LatLng || ","
     }
@@ -171,19 +78,19 @@ namespace GMH.Util {
    * Converts a custom formatted string into an array of LatLng objects 
    * @param str A custom formatted string of x and y coordinate pairs 
    */
-  export function toLatLngArray(str: string) {
+  export function toLatLngArray(str: string): google.maps.LatLng[] {
     const Delimeter = {
       LatLng: GMH.Config.Delimeter.LatLng || ",",
       LatLngPair: GMH.Config.Delimeter.LatLngPair || "|"
     }
 
-    const latLngArray = [];
-    const coordPairs = str.split(Delimeter.LatLngPair);
+    const latLngArray = []
+    const coordPairs = str.split(Delimeter.LatLngPair)
 
     for (var i = 0, i_end = coordPairs.length; i < i_end; i++) {
       let points = coordPairs[i].split(Delimeter.LatLng)
       let latLng = new google.maps.LatLng(parseFloat(points[0]), parseFloat(points[1]))
-      latLngArray.push(latLng);
+      latLngArray.push(latLng)
     }
 
     return latLngArray;
@@ -193,18 +100,18 @@ namespace GMH.Util {
    * Returns correct ObjectType constant
    * @param type A string of the object type 
    */
-  export function getObjectType(type: string) {
-    type = type.toLowerCase()
-    return ObjectTypeAlias[type]
+  export function getObjectType(type: string): string {
+    type = type.toLowerCase().replace(/\s+/g, '')
+    return _C.Object.Alias[type] || type
   }
 
   /**
    * Returns correct EventType constant
    * @param event A string of the event type 
    */
-  export function getEventType(event: string) {
+  export function getEventType(event: string): string {
     event = event.toLowerCase().replace(/\s+/g, '')
-    return EventTypeAlias[event] || event
+    return _C.Event.Alias[event] || event
   }
 }
 
