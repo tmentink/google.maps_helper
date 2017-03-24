89ac7e84235a266f0610bfc3405d2cf7177759db
Trent Mentink <trentmentink@gmail.com>
2017-03-24 12:09:41 -0700

    updated map constructor to create google map object, use constant namespace and save initial options and bounds.

--- src/ts/obj/map.ts
+++ src/ts/obj/map.ts
@@ -6,23 +6,31 @@ namespace GMH.Obj {
 
 
   // ----------------------------------------------------------------------
-  // Constants 
-  // ----------------------------------------------------------------------
-  
-  const TYPE = "Map"
-
-
-  // ----------------------------------------------------------------------
   // Class Definition 
   // ----------------------------------------------------------------------
 
   export class Map {
+    Init: {
+      Bounds: google.maps.LatLngBounds
+      Options: google.maps.MapOptions
+    }
+    Type: string
     Obj: google.maps.Map
-    Type: string 
 
-    constructor(obj:google.maps.Map) {
-      this.Obj = obj
-      this.Type = TYPE
+    constructor(containerID:string, options: {}) {
+      this.Init = {
+        Bounds: undefined,
+        Options: options
+      }
+      this.Type = _C.Object.Type.MAP
+      this.Obj = new google.maps.Map(document.getElementById(containerID), options)
+      this.Obj["GMH"] = { Parent: function() { return GMH.$.Map } }
+
+      // Save bounds after map has finished loading
+      google.maps.event.addListenerOnce(this.Obj, _C.Event.Type.TILES_LOADED, () =>{
+        this.Init.Bounds = this.Obj.getBounds()
+      })
     }
   }
 }
+
