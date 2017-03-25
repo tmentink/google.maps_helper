6c0d88e080cd5bacb6634f3ab4578f798fe7c551
Trent Mentink <trentmentink@gmail.com>
2017-03-25 15:06:07 -0700

    added listener functions to marker namespace

---
+++ src/ts/marker/marker--listener.ts
@@ -0,0 +1,105 @@
+/// <reference path="../obj/marker.ts" />
+/// <reference path="../util.ts" />
+/// <reference path="../data.ts" />
+
+// ------------------------------------------------------------------------
+// Google Maps Helper v1.0.0: marker--listener.ts 
+// ------------------------------------------------------------------------
+
+namespace GMH.Marker {
+
+
+  // ----------------------------------------------------------------------
+  // Constants 
+  // ----------------------------------------------------------------------
+  const Action = {
+    ADD         : "add", 
+    REMOVE_ALL  : "remove_all",
+    REMOVE_TYPE : "remove_type",
+  }
+
+  const Execute = {
+    add         : function(id, type, fn) { return _add(id, type, fn) },
+    remove_all  : function(id, type, fn) { return _removeAll(id) },
+    remove_type : function(id, type, fn) { return _removeType(id, type) }
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Public Functions 
+  // ----------------------------------------------------------------------
+  
+  /**
+   * Adds an event listener to the marker(s)
+   * @param id The id of the marker. Can also be an array of ids
+   * @param type The event type
+   * @param fn The function to be executed when event is triggered 
+   */
+  export function addListener(id: string | string[], type: string, fn: Function): Obj.Marker | Obj.MarkerArray {
+    type = Util.getEventType(type)
+    return _listener(id, type, fn, Action.ADD)
+  }
+
+  /**
+   * Removes all listeners from the marker(s)
+   * @param id The id of the marker. Can also be an array of ids
+   */
+  export function removeAllListeners(id: string | string[]): Obj.Marker | Obj.MarkerArray {
+    return _listener(id, null, null, Action.REMOVE_ALL)
+  }
+
+  /**
+   * Removes all listeners of the given type from the marker(s)
+   * @param id The id of the marker. Can also be an array of ids
+   * @param type The event type
+   */
+  export function removeListenerType(id: string | string[], type: string): Obj.Marker | Obj.MarkerArray {
+    type = Util.getEventType(type)
+    return _listener(id, type, null, Action.REMOVE_TYPE)
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Private Functions 
+  // ----------------------------------------------------------------------
+  
+  function _listener(id: string | string[], type: string, fn: Function, action: string): Obj.Marker | Obj.MarkerArray {
+    if (jQuery.isArray(id)) {
+      return _multiListener(id, type, fn, action)
+    }
+
+    if ($.Marker[id]) {
+      return Execute[action](id, type, fn)
+    }
+  }
+
+  function _multiListener(ids: string[], type: string, fn: Function, action: string): Obj.MarkerArray {
+    const markerArray = new Obj.MarkerArray()
+
+    for (var i = 0, i_end = ids.length; i < i_end; i++) {
+      let id = ids[i]
+      if ($.Marker[id]) {
+        markerArray[id] = Execute[action](id, type, fn)
+      }
+    }
+
+    return markerArray
+  }
+
+  function _add(id: string, type: string, fn: Function): Obj.Marker {
+    google.maps.event.addListener($.Marker[id].Obj, type, fn)
+    return $.Marker[id]
+  }
+
+  function _removeType(id: string, type: string): Obj.Marker {
+    google.maps.event.clearListeners($.Marker[id].Obj, type)
+    return $.Marker[id]
+  }
+
+  function _removeAll(id: string): Obj.Marker {
+    google.maps.event.clearInstanceListeners($.Marker[id].Obj)
+    return $.Marker[id]
+  }
+  
+}
+
