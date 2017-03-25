c6877cfc40473481a41ec775faf212a2013f2a3c
Trent Mentink <trentmentink@gmail.com>
2017-03-25 15:00:47 -0700

    added bounds functions to marker namespace

---
+++ src/ts/marker/marker--bounds.ts
@@ -0,0 +1,55 @@
+/// <reference path="../obj/marker.ts" />
+/// <reference path="../util.ts" />
+/// <reference path="../data.ts" />
+
+// ------------------------------------------------------------------------
+// Google Maps Helper v1.0.0: marker--bounds.ts 
+// ------------------------------------------------------------------------
+
+namespace GMH.Marker {
+
+
+  // ----------------------------------------------------------------------
+  // Public Functions 
+  // ----------------------------------------------------------------------
+  
+  /**
+   * Returns the bounds of the marker(s)
+   * @param ids The id of the marker. Can also be an array of ids
+   */
+  export function getBounds(ids: string | string[]): google.maps.LatLngBounds {
+    ids = Util.toArray(ids)
+
+    const bounds = new google.maps.LatLngBounds();
+    for (var i = 0, i_end = ids.length; i < i_end; i++) {
+      let id = ids[i]
+      
+      if ($.Marker[id]) { 
+        bounds.union(_getMarkersBounds(id))
+      }
+    }
+
+    return bounds
+  }
+
+  /**
+   * Returns the center LatLng object of the marker(s) bounds
+   * @param ids The id of the marker. Can also be an array of ids
+   */
+  export function getCenter(id: string | string[]): google.maps.LatLng {
+    return getBounds(id).getCenter()
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Private Functions 
+  // ----------------------------------------------------------------------
+  
+  function _getMarkersBounds(id) {
+    const bounds = new google.maps.LatLngBounds()
+    bounds.extend($.Marker[id].Obj.getPosition())
+    return bounds
+  }
+
+}
+
