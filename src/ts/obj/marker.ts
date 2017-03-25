8ebbb00b9aea5b3186fe8db80b346141ded5fd78
Trent Mentink <trentmentink@gmail.com>
2017-03-25 15:34:55 -0700

    added all public methods to marker class

--- src/ts/obj/marker.ts
+++ src/ts/obj/marker.ts
@@ -31,6 +31,63 @@ namespace GMH.Obj {
       }
       this.Type = _C.Object.Type.MARKER
     }
+
+
+    // ----------------------------------------------------------------------
+    // Public Methods 
+    // ----------------------------------------------------------------------
+
+    public addListener(type: string, fn: Function) {
+      return GMH.Marker.addListener(this.ID, type, fn)
+    }
+
+    public getBounds() {
+      return GMH.Marker.getBounds(this.ID)
+    }
+
+    public getCenter() {
+      return GMH.Marker.getCenter(this.ID)
+    }
+
+    public hide() {
+      return GMH.Marker.hide(this.ID)
+    }
+
+    public not() {
+      return GMH.Util.copy($.Marker, this.ID)
+    }
+
+    public remove() {
+      return GMH.Marker.remove(this.ID)
+    }
+
+    public removeAllListeners() {
+      return GMH.Marker.removeAllListeners(this.ID)
+    }
+
+    public removeListenerType(type: string) {
+      return GMH.Marker.removeListenerType(this.ID, type)
+    }
+
+    public reset() {
+      return GMH.Marker.reset(this.ID)
+    }
+
+    public show() {
+      return GMH.Marker.show(this.ID)
+    }
+
+    public toggle() {
+      return GMH.Marker.toggle(this.ID)
+    }
+
+    public update(options: google.maps.MarkerOptions) {
+      return GMH.Marker.update(this.ID, options)
+    }
+
+    public updatePosition(position: any) {
+      return GMH.Marker.updatePosition(this.ID, position)
+    }
   }
 
 }
