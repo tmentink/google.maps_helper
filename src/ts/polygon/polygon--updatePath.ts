917fcaa105a12fe709c2a9222f7f0913dac0386a
Trent Mentink <trentmentink@gmail.com>
2017-03-25 11:56:34 -0700

    added updatePath function to polygon namespace

---
+++ src/ts/polygon/polygon--updatePath.ts
@@ -0,0 +1,75 @@
+/// <reference path="../obj/polygon.ts" />
+/// <reference path="../util.ts" />
+/// <reference path="../data.ts" />
+
+// ------------------------------------------------------------------------
+// Google Maps Helper v1.0.0: polygon--updatePath.ts 
+// ------------------------------------------------------------------------
+
+namespace GMH.Polygon {
+
+
+  // ----------------------------------------------------------------------
+  // Public Functions 
+  // ----------------------------------------------------------------------
+  
+  /**
+   * Updates the polygon's path 
+   * @param id The id of the polygon. Can also be an array of ids
+   * @param path The path of the polygon. Can be a string or a LatLngArray 
+   */
+  export function updatePath(id: string | string[], path: any): Obj.Polygon | Obj.PolygonArray {
+    if (jQuery.type(path) == "string") {
+      path = Util.toLatLngArray(path)
+    }
+
+    return _updatePath(id, path)
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Private Functions 
+  // ----------------------------------------------------------------------
+  
+  function _updatePath(id: string | string[], path: any): Obj.Polygon | Obj.PolygonArray {
+    if (jQuery.isArray(id)) {
+      return _multiUpdatePath(id, path);
+    }
+
+    if (_validParameters(id, path)) {
+      return _updatePolygonPath(id, path)
+    }
+  }
+
+  function _multiUpdatePath(ids: string[], path: any): Obj.PolygonArray {
+    const polygonArray = new Obj.PolygonArray()
+
+    for (var i = 0, i_end = ids.length; i < i_end; i++) {
+      let id = ids[i]
+
+      if (_validParameters(id, path)) {
+        polygonArray[id] = _updatePolygonPath(id, path)
+      }
+    }
+
+    return polygonArray
+  }
+
+  function _updatePolygonPath(id: string, path: any): Obj.Polygon {
+    $.Polygon[id].Obj.setOptions({"paths": path});
+    return $.Polygon[id];
+  }
+
+  function _validParameters(id: any, path: any): boolean {
+    if ($.Polygon[id]) {
+      throw "Error: ID does not exist"
+    }
+    if (!path) {
+      throw "Error: Must supply a path"
+    }
+
+    return true
+  }
+
+}
+
