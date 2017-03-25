331465cbbbf7212dd9c9d71e679dc88779136265
Trent Mentink <trentmentink@gmail.com>
2017-03-25 15:30:28 -0700

    added update function to marker namespace

---
+++ src/ts/marker/marker--update.ts
@@ -0,0 +1,70 @@
+/// <reference path="../obj/marker.ts" />
+/// <reference path="../util.ts" />
+/// <reference path="../data.ts" />
+
+// ------------------------------------------------------------------------
+// Google Maps Helper v1.0.0: marker--update.ts 
+// ------------------------------------------------------------------------
+
+namespace GMH.Marker {
+
+
+  // ----------------------------------------------------------------------
+  // Public Functions 
+  // ----------------------------------------------------------------------
+  
+  /**
+   * Updates the marker's options 
+   * @param id The id of the marker. Can also be an array of ids
+   * @param userOptions If options are null, the defaults in GMH.Config are used
+   */
+  export function update(id: string | string[], userOptions: google.maps.MarkerOptions): Obj.Marker | Obj.MarkerArray {
+    return _update(id, _getOptions(userOptions))
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Private Functions 
+  // ----------------------------------------------------------------------
+  
+  function _update(id: string | string[], options: google.maps.MarkerOptions): Obj.Marker | Obj.MarkerArray {
+    if (jQuery.isArray(id)) {
+      return _multiUpdate(id, options)
+    }
+
+    if ($.Marker[id]) {
+      return _updateMarker(id, options)
+    }
+  }
+
+  function _multiUpdate(ids: string[], options: google.maps.MarkerOptions): Obj.MarkerArray {
+    const markerArray = new Obj.MarkerArray()
+
+    for (var i = 0, i_end = ids.length; i < i_end; i++) {
+      let id = ids[i]
+      if ($.Marker[id]) {
+        markerArray[id] = _updateMarker(id, options)
+      }
+    }
+
+    return markerArray
+  }
+
+  function _updateMarker(id: string, options: google.maps.MarkerOptions): Obj.Marker {
+    $.Marker[id].Obj.setOptions(options);
+    return $.Marker[id];
+  }
+
+  function _getOptions(options: any): google.maps.MarkerOptions {
+    const defaults = Config.Default.Marker || {}
+    options = options == null ? defaults : options
+
+    if (jQuery.type(options.position) == "string") {
+      options.position = Util.toLatLng(options.position);
+    }
+
+    return options
+  }
+  
+}
+
