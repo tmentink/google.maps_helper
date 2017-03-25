af97edbffcd2f86895ac3077bf32d4649c8e090f
Trent Mentink <trentmentink@gmail.com>
2017-03-25 15:30:40 -0700

    added reset function to marker namespace

---
+++ src/ts/marker/marker--reset.ts
@@ -0,0 +1,58 @@
+/// <reference path="../obj/marker.ts" />
+/// <reference path="../util.ts" />
+/// <reference path="../data.ts" />
+
+// ------------------------------------------------------------------------
+// Google Maps Helper v1.0.0: marker--reset.ts 
+// ------------------------------------------------------------------------
+
+namespace GMH.Marker {
+
+
+  // ----------------------------------------------------------------------
+  // Public Functions 
+  // ----------------------------------------------------------------------
+  
+
+  /**
+   * Resets the marker to its initialized state
+   * @param id The id of the marker. Can also be an array of ids
+   */
+  export function reset(id: string | string[]): Obj.Marker | Obj.MarkerArray {
+    return _reset(id)
+  }
+
+
+  // ----------------------------------------------------------------------
+  // Private Functions 
+  // ----------------------------------------------------------------------
+
+  function _reset(id: string | string[]): Obj.Marker | Obj.MarkerArray {
+    if (jQuery.isArray(id)) {
+      return _multiReset(id)
+    }
+
+    if ($.Marker[id]) {
+      return _resetMarker(id)
+    }
+  }
+
+  function _multiReset(ids: string[]): Obj.MarkerArray {
+    const markerArray = new Obj.MarkerArray()
+
+    for (var i = 0, i_end = ids.length; i < i_end; i++) {
+      let id = ids[i]
+      if ($.Marker[id]) {
+        markerArray[id] = _resetMarker(id)
+      }
+    }
+
+    return markerArray
+  }
+
+  function _resetMarker(id: string): any {
+    return GMH.Marker.update(id, $.Marker[id].Init.Options);
+  }
+ 
+}
+
