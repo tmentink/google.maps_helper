897ac0423f7ac9037cca50dd65e729eae42cde4a
Trent Mentink <trentmentink@gmail.com>
2017-03-25 16:42:43 -0700

    added all public methods to label and labelArray class

--- src/ts/obj/label.ts
+++ src/ts/obj/label.ts
@@ -32,6 +32,50 @@ namespace GMH.Obj {
       this.Type = _C.Object.Type.LABEL
     }
 
+
+    // ----------------------------------------------------------------------
+    // Public Methods 
+    // ----------------------------------------------------------------------
+
+    public getBounds() {
+      return GMH.Label.getBounds(this.ID)
+    }
+
+    public getCenter() {
+      return GMH.Label.getCenter(this.ID)
+    }
+
+    public hide() {
+      return GMH.Label.hide(this.ID)
+    }
+
+    public not() {
+      return GMH.Util.copy($.Label, this.ID)
+    }
+
+    public remove() {
+      return GMH.Label.remove(this.ID)
+    }
+
+    public reset() {
+      return GMH.Label.reset(this.ID)
+    }
+
+    public show() {
+      return GMH.Label.show(this.ID)
+    }
+
+    public toggle() {
+      return GMH.Label.toggle(this.ID)
+    }
+
+    public update(options: IGoogleLabelOptions) {
+      return GMH.Label.update(this.ID, options)
+    }
+
+    public updatePosition(position: any) {
+      return GMH.Label.updatePosition(this.ID, position)
+    }
   }
 
 }
